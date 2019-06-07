57d92f847e09d0be6a1bee198dfc8ee22bbc92aa
srodte2006 <taabay@student.rtc.edu>
2019-06-07 05:44:46 -0700

    Incorporated imolemenation of the web layer to CyclicGraphDetectorApplication solution.

---
+++ CyclicGraphDetectorWebApp/Models/CsvFileContent.cs
@@ -0,0 +1,10 @@
+﻿namespace CyclicGraphDetectorWebApp.Models
+{
+    public class CsvFileContentModel
+    {
+        //  public List<CsvEntry> CsvEntries { get; set; }
+        //  public Dictionary<string,HashSet<WeightedChildNode>> ParentsWithChildrenAdjecencyList { get; set; }
+        public int Id { get; set; }
+        public string FileName { get; set; }
+    }
+}
